b3cb67bbcc37efb93d66702f09a0710dadb049bd
Ijasdeen <ijasdeen23@gmail.com>
2020-11-04 19:21:07 +0530

    change something in the home

--- admin_panel/application/views/home/home.php
+++ admin_panel/application/views/home/home.php
@@ -68,9 +68,27 @@
                        		<h5 class="text-white">First caraousal</h5>
                        	</div>
                        	<div class="card-body">
-                       		<form>
+                       		<form id="frm_firstcaraousals">
                        			<div class="form-group py-2">
-                       			<input type="file" class="form-control">
+                       			<input type="file" class="form-control" id="first_caraousals">
+                       			</div>
+                       			<div class="form-group">
+                       				<input type="submit" class="form-control btn btn-outline-primary" value="Upload">
+                       			</div>
+                       		</form>
+                       	</div>
+                       </div>
+                    </div>
+                    
+                       <div class="col-md-4">
+                       <div class="card bg-dark text-white">
+                       	<div class="card-header bg-info">
+                       		<h5 class="text-white">Seond caraousal</h5>
+                       	</div>
+                       	<div class="card-body">
+                       		<form id="frm_secondcaraousals">
+                       			<div class="form-group py-2">
+                       			<input type="file" class="form-control" id="second_caraousals">
                        			</div>
                        			<div class="form-group">
                        				<input type="submit" class="form-control btn btn-outline-primary" value="Upload">
