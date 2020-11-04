da876b13f4c6af0db9c9092a272aa52f4e8371a9
Ijasdeen <ijasdeen23@gmail.com>
2020-11-04 19:16:26 +0530

    Editing home

--- admin_panel/application/views/home/home.php
+++ admin_panel/application/views/home/home.php
@@ -63,7 +63,21 @@
             <div class="card-body">
                 <div class="row">
                       <div class="col-md-4">
-                       
+                       <div class="card bg-dark text-white">
+                       	<div class="card-header bg-info">
+                       		<h5 class="text-white">First caraousal</h5>
+                       	</div>
+                       	<div class="card-body">
+                       		<form>
+                       			<div class="form-group py-2">
+                       			<input type="file" class="form-control">
+                       			</div>
+                       			<div class="form-group">
+                       				<input type="submit" class="form-control btn btn-outline-primary" value="Upload">
+                       			</div>
+                       		</form>
+                       	</div>
+                       </div>
                     </div>
                 </div>
             </div>
